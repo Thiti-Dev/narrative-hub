fd7a02fe4c8ce57029676da1980e74252226357c
Thiti-Dev <thiti.mwk.main@gmail.com>
2023-09-17 00:39:18 +0700

    extended DefaultState to has the user property which has the claims-data of the jwt token

--- @types/koa.d.ts
+++ @types/koa.d.ts
@@ -4,4 +4,14 @@ declare module "koa" {
     interface Request {
         dto?: unknown
     }
+
+    interface DefaultState{
+        user: {
+            username: string;
+            name: string;
+            id: number;
+            iat: number;
+            exp: number;
+        }
+    }
 }
