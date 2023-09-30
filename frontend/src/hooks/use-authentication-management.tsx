b6e4f0f8c235fdb2cf9acae31fa76bbd00efc861
Thiti-Dev <thiti.mwk.main@gmail.com>
2023-09-30 10:15:41 +0700

    properly initializing token state from localStorage

--- frontend/src/hooks/use-authentication-management.tsx
+++ frontend/src/hooks/use-authentication-management.tsx
@@ -1,25 +1,28 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import jwt_decode from "jwt-decode";
 import { IUserData } from '../shared/types/authentication/common';
 import { useNavigate } from 'react-router-dom';
 import { useAuthenticationStore } from '../states/authentication';
 
 export default function useAuthenticationManagement() {
+  const [localStorageValue, setLocalStorageValue] = useState<string|null>(localStorage.getItem('token'));
   useEffect(() => {
-    const token = localStorage.getItem('token')
-
-    if(token){
+    if(localStorageValue){
       // if found token
-      const decoded = jwt_decode<IUserData>(token)
-    
-      if(+new Date()/1000 >= decoded.exp){
+      
+      try {
+        const decoded = jwt_decode<IUserData>(localStorageValue)
+        if(+new Date()/1000 >= decoded.exp){
+          localStorage.removeItem('token')
+          window.location.href = "/master-verification"
+        }
+        useAuthenticationStore.setState({isAuthenticated:true,userData: decoded})
+      } catch {
         localStorage.removeItem('token')
         window.location.href = "/master-verification"
       }
 
-      useAuthenticationStore.setState({isAuthenticated:true,userData: decoded})
-
     }
-  },[])
+  },[localStorageValue])
   return null
 }
